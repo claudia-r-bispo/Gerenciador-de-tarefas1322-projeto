d90a9339dedc1e36cb7c840bff5e5f3d6876acce
BrunoFerreira1990 <111455462+BrunoFerreira1990@users.noreply.github.com>
2025-04-13 12:07:14 -0300

    Subindo arquivos impl

--- projeto/src/br/com/ada/t1322/tecnicasprogramacao/projeto/Application.java
+++ projeto/src/br/com/ada/t1322/tecnicasprogramacao/projeto/Application.java
@@ -1,6 +1,7 @@
 package br.com.ada.t1322.tecnicasprogramacao.projeto;
 
 import br.com.ada.t1322.tecnicasprogramacao.projeto.controller.TaskController;
+import br.com.ada.t1322.tecnicasprogramacao.projeto.controller.TaskControllerImpl;
 import br.com.ada.t1322.tecnicasprogramacao.projeto.repository.TaskRepositoryImpl;
 import br.com.ada.t1322.tecnicasprogramacao.projeto.service.TaskServiceImpl;
 import br.com.ada.t1322.tecnicasprogramacao.projeto.util.SampleDataInitializer;
